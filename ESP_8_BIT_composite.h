36382f05debb8d14aa614f30f900eb670738afef
Roger Cheng <roger.random@outlook.com>
2021-04-24 02:28:11 -0700

    First draft in Arduino library form

--- ESP_8_BIT_composite.h
+++ ESP_8_BIT_composite.h
@@ -3,16 +3,36 @@
 
 #include "Arduino.h"
 
+#include "esp_types.h"
 #include "esp_err.h"
 #include "esp_log.h"
+
+#include "esp_attr.h"
+#include "esp_heap_caps.h"
+#include "esp_intr_alloc.h"
+#include "driver/periph_ctrl.h"
+#include "driver/dac.h"
+#include "driver/gpio.h"
+#include "driver/i2s.h"
+#include "rom/gpio.h"
+#include "rom/lldesc.h"
+#include "soc/gpio_reg.h"
+#include "soc/i2s_struct.h"
+#include "soc/i2s_reg.h"
+#include "soc/io_mux_reg.h"
+#include "soc/rtc.h"
+#include "soc/rtc_io_reg.h"
+#include "soc/soc.h"
 
 class ESP_8_BIT_composite
 {
   public:
     ESP_8_BIT_composite(int ntsc);
-    void print_hello();
+    void setup();
+    void vsync();
+    uint8_t** get_lines();
   private:
-    int _ntsc;
+    void instance_check();
 };
 
 #endif // ESP_8_BIT_COMPOSITE_H
